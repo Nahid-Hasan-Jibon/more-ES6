811db92aa78cb7a17ce0d32060e8070d8234aa86
Nahid-Hasan-Jibon <smnahidhasanjibon@gmail.com>
2024-12-15 02:16:54 +0600

    find added

---
+++ find.js
@@ -0,0 +1,7 @@
+// filter er khalato vai. but condion jodi ekadhik jon mane tahle se sudhu prothom jon kei return korbe . keu sorto na manle udefined dibe
+// filter array dibe find array dibe na
+const players = [56, 67, 78, 98, 34, 56, 75, 78, 59];
+
+
+const selected = players.find(player=>player>70)
+console.log(selected)
