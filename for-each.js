f3efbe0cc96f8875989ec4bfd711a0422867e668
Nahid-Hasan-Jibon <smnahidhasanjibon@gmail.com>
2024-12-15 01:54:15 +0600

    forEach added

---
+++ for-each.js
@@ -0,0 +1,7 @@
+//  for each works like map but map return somthing but for each does not return something
+
+const numbers = [1, 2, 5, 6, 7, 8, 15];
+
+const result = numbers.forEach((n) => console.log(n));
+
+console.log(result)
